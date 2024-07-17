9c690f74e9d25ba9c530281c9e866b261451e4cb
Mmaphokeng Senne <125877787+Fabphokie@users.noreply.github.com>
2024-07-17 23:16:18 +0200

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,24 @@
 export default function HomePage() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-cream-white p-4">
-      <h1 className="text-4xl font-bold mb-4">Welcome to Kitso!</h1>
-      <p className="text-lg mb-6">Your ultimate destination for knowledge, growth, and innovation.</p>
+      <div className="carousel mb-4">
+        <div className="carousel-text text-4xl font-bold">
+          <span>Welcome to Kitso! </span>
+          
+          
+        </div>
+      </div>
+      <p className="text-lg mb-6 text-center">
+        Your ultimate destination for knowledge, growth, and innovation.
+      </p>
       <div style={{ width: "100%" }}>
         <div style={{ height: 0, paddingBottom: "56.25%", position: "relative", width: "100%" }}>
           <iframe
             allowFullScreen
             frameBorder="0"
-            height="100%"
+            height="600"
             src="https://giphy.com/embed/CHJAZ8hZUsH3yyBw4q/video"
-            style={{ left: 0, position: "absolute", top: 0 }}
+            style={{ height: '600px' }}
             width="100%"
           ></iframe>
         </div>
