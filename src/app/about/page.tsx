d848f9d1625103da1d01607027b3cee128a4fef6
Mmaphokeng Senne <125877787+Fabphokie@users.noreply.github.com>
2024-07-17 20:32:19 +0200

    wip

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+// src/app/about/page.tsx
+import React from 'react';
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
+}
