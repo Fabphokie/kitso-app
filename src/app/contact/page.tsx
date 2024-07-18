49d489c7558cc0402298d2f32bb36fdcd41ca2c7
Mmaphokeng Senne <125877787+Fabphokie@users.noreply.github.com>
2024-07-18 11:12:53 +0200

    WIP

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+// src/app/about/page.tsx
+import React from 'react';
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>This is the contact page.</p>
+    </div>
+  );
+}
