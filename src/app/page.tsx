8eebdb685a5e810951a34bdfb1dcd3963b9289b7
Mmaphokeng Senne <125877787+Fabphokie@users.noreply.github.com>
2024-07-19 14:07:03 +0200

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,11 @@ export default function HomePage() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-cream-white p-4">
       <div className="carousel mb-4">
-        <div className="carousel-text text-5xl font-bold text-pastel-green animate-marquee">
-          <span>Welcome to Kitso! </span>
+        <div className="carousel-text text-4xl font-bold text-pastel-green animate-marquee">
+          <span> Knowledge | Growth | Innovation </span>
           
         </div>
       </div>
-      <p className="text-lg mb-6 text-center text-pastel-green">
-         Knowledge | Growth | Innovation
-      </p>
        <QueryComponent/>
       <div className="w-full max-w-4xl">
         <div className="relative pb-9/16">
