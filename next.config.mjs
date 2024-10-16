d1e18907c9d0beb3c68e0abdc3d20ebfbc12a668
Mmaphokeng Senne <mmaphokengsenne@gmail.com>
2024-10-16 12:18:47 +0200

    WIP

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['img.freepik.com', 'encrypted-tbn0.gstatic.com'], // Add the domains here
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'img.freepik.com',
+          port: '',
+        },
+        {
+          protocol: 'https',
+          hostname: 'encrypted-tbn0.gstatic.com',
+          port: '',
+        },
+        {
+          protocol: 'https',
+          hostname: 'i.pinimg.com',
+          port: '',
+        },
+        {
+          protocol: 'https',
+          hostname: 'media.istockphoto.com', // Added for istock images
+          port: '',
+        },
+        // Add any other image sources you need here
+      ],
     },
   };
   
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,27 @@
+// src/components/Header.tsx
 import Link from "next/link";
 import DropDownButton from "./DropDownButton";
 
 export default function Header() {
-  const topics = ['Puberty', 'Pregnancy', 'Family Planning', 'Mental wellness'];
-  const logoUrl = "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExbTY0bGd3bXcxNXA2djdrampna2pmN2xsd2VqYjd5c2VucGZuM3JmMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/j1ODNPrvSVeyThBe3b/giphy.gif"; // GIF URL
+  const topics = ['Puberty', 'Pregnancy', 'Family Planning', 'Mental Wellness'];
+  const logoUrl = "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExbTY0bGd3bXcxNXA2djdrampna2pmN2xsd2VqYjd5c2VucGZuM3JmMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/j1ODNPrvSVeyThBe3b/giphy.gif";  // GIF URL
 
   return (
     <header className="bg-pastel-purple text-white p-4">
       <nav className="container mx-auto flex justify-between items-center">
         <div className="text-2xl font-bold flex items-center">
           <Link href="/" className="flex items-center">
-            <img 
-              src={logoUrl} 
-              alt="Kitso Logo - Animated logo representing the Kitso brand" 
-              className="w-12 h-auto ml-2" 
-            />
-            <span className="ml-2">K i t s o.</span>
+            <img src={logoUrl} alt="Kitso Logo" className="w-12 h-auto ml-2" />  
+            <span>K i t s o.</span>
           </Link>
         </div>
         <div>
           <DropDownButton topics={topics} />
         </div>
-        <div className="space-x-4">
-          <Link href="/" className="hover:underline">Home</Link>
-          <Link href="/about" className="hover:underline">About</Link>
-          <Link href="/contact" className="hover:underline">Contact</Link>
+        <div className="space-x-4 ">
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/contact">Contact</Link>
         </div>
       </nav>
     </header>
